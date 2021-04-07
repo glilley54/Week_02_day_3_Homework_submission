7132637d6ca9c3ca657e31c41ca5de6b913243eb
glilley54 <glilley54@gmail.com>
2021-04-07 19:03:34 +0100

    about to start task

--- start_code/src/compare.py
+++ start_code/src/compare.py
@@ -1,3 +1,9 @@
 def compare(num_1, num_2):
     return f"{num_1} is greater than {num_2}"
     
+def compare(num_1, num_2):
+    # ADDED
+    if num_1 < num_2:
+        return f"{num_1} is less than {num_2}"
+    #
+    return f"{num_1} is greater than {num_2}"
